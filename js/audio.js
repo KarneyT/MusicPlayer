14215698890a3d6b0a2f64afb6abd329790662fd
KarneyT <35508748+KarneyT@users.noreply.github.com>
2018-03-26 19:26:55 -0400

    Files
    Files Added

---
+++ js/audio.js
@@ -0,0 +1,91 @@
+﻿var player;
+var rngVolume;
+var intv;
+
+window.onload = function(){
+document.addEventListener('deviceready', init, false);
+}
+
+function init()
+{
+	player = document.getElementById('player');
+	var btnPlay = document.getElementById('btnPlay');
+	var btnPause = document.getElementById('btnPause');
+	var btnStop = document.getElementById('btnStop');
+	rngVolume = document. getElementById('rngVolume');
+	
+	btnPlay.addEventListener ('click', playMusic, false);
+	btnPause.addEventListener('click', pauseMusic, false);
+	btnStop.addEventListener('click', stopMusic, false);
+	}
+	
+function playMusic()
+{
+player.play();
+startTimer();
+}
+
+function pauseMusic()
+{
+player.pause();
+}
+
+function stopMusic()
+{
+pauseMusic();
+player.currentTime = 0;
+stopTimer();
+}
+
+function changeVolume()
+{
+player.volume = rngVolume.value;
+}
+
+function changeSong(song)
+{
+stopMusic();
+player.src = "assets/" + song + ".mp3";
+playMusic();
+}
+
+function startTimer()
+{
+intv = setInterval(updateTime, 1000);
+}
+
+function stopTimer()
+{
+clearInterval(intv);
+updateTime();
+}
+function updateTime()
+{
+document.getElementById('timeOut').innerHTML = "Elapsed Time: " + secsToMins(player.currentTime);
+}
+
+function secsToMins(seconds)
+{
+var minutes = Math.floor(seconds/60);
+	var theSeconds = seconds - minutes * 60;
+		if(theSeconds > 9)
+		{
+			return minutes + ":" + Math.round(theSeconds);
+			} else
+			{
+				return minutes + ":0" + Math.round(theSeconds);
+				}
+			}
+			
+function secsToMins(seconds)
+{
+var minutes = Math.floor(seconds/60);
+var theSeconds = seconds - minutes * 60;
+	if(theSeconds > 9)
+	{
+		return minutes + ":" + Math.round(theSeconds);
+		} else
+		{
+			return minutes + ":0" + Math.round(theSeconds);
+			}
+		}
---
+++ js/main.js
@@ -0,0 +1,35 @@
+﻿//<script type= "application/javascript">
+//window.onload = function(){
+	//document.getElementById("btnMove").addEventListener("click", change, false);
+//}
+
+//function change() {
+//	window.location = "secondPage.html";
+//	}
+	
+//</script>
+	
+var storyOne = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet erat risus. Etiam aliquet sed libero ac condimentum. Sed in pretium arcu.</p>";
+ var storyTwo ="HEY";
+   
+ window.onload = function(){
+ 	document.getElementById("btnOne").addEventListener("click", postStoryOne, false);
+ 		document.getElementById("btnTwo").addEventListener("click", postStoryTwo, false);
+ 		}
+ 		
+ function postStoryOne(){
+ 	document.getElementById("content").innerHTML = storyOne;
+ 	document.getElementById("title").innerHTML = "<h2>Story One!</h2>";
+ 	}
+ 	
+ function postStoryTwo(){
+ 	document.getElementById("content").innerHTML = storyTwo;
+ 	document.getElementById("title").innerHTML = "<h2>Story Two!</h2>";
+ 	}
+ 	
+ function postStoryOne(){
+ 	document.getElementById("content").innerHTML = storyOne;
+ 	document.getElementById("title").innerHTL = "<h2>Story One!</h2>";
+ 	document.getElementById("btnOne").style.display = "none";
+ 	document.getElementById("btnTwo").style.display = "block";
+ 	}
